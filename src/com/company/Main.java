3d51fc45f9d216ac6b2e93655d27fa02b576ec40
IrinaYarkova <irina.yarkova.1996@mail.ru>
2019-07-11 21:18:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        String s;
+        try(BufferedReader br = new BufferedReader (new FileReader("text.txt")))
+        {
+            while((s = br.readLine()) != null){
+                System.out.println(s);
+            }
+        }catch (IOException exc){
+            System.out.println("Ошибка ввода-вывода: " + exc);
+        }
+    }
+}
